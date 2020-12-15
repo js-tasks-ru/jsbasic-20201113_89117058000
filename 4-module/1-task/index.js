22225c9721b0e6869a203142708e37327dc5a5ab
anton-iavorskii <89117058000@mail.ru>
2020-12-15 10:38:00 +0300

    add function makeFriendsList

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,5 +4,19 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
-  // ваш код...
+  let arr = [];
+  friends.forEach((elem) => {
+    arr.push(elem.firstName + ' ' + elem.lastName)
+  })
+
+  let ul = document.createElement('UL');
+  
+  arr.forEach((elem) => {
+  let li = document.createElement('LI');
+  li.textContent = elem;
+  ul.appendChild(li)
+
+  })
+  
+  return ul
 }
