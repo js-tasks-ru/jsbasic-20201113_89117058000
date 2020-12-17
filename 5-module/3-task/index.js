81c437c55b26b4d8c196ca96a734bd39cb332cd8
anton-iavorskii <89117058000@mail.ru>
2020-12-17 10:46:36 +0300

    add function initCarousel - module-5

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,29 @@
 function initCarousel() {
-  // ваш код...
+  let btnRight = document.querySelector('.carousel__arrow_right');
+  let btnLeft = document.querySelector('.carousel__arrow_left');
+  let carouselInner = document.querySelector('.carousel__inner'); 
+  let carouselArrowRight = document.querySelector('.carousel__arrow_right');
+  let carouselArrowLeft = document.querySelector('.carousel__arrow_left')
+  let slideLen = document.querySelectorAll('.carousel__slide').length;
+  let slide = document.querySelector('.carousel__slide');
+  let slideWidth = slide.offsetWidth - slide.offsetWidth
+  carouselArrowLeft.style.display = 'none'
+
+  btnRight.addEventListener('click', function() {
+    carouselArrowLeft.style.display = ''
+    slideWidth = slideWidth - slide.offsetWidth;
+    carouselInner.style.transform = `translateX(${slideWidth}px)`;
+    if(slideWidth == (-slide.offsetWidth * slideLen)+slide.offsetWidth) {
+      carouselArrowRight.style.display = 'none'
+    }
+  })
+
+  btnLeft.addEventListener('click', function() {
+    slideWidth = slideWidth + slide.offsetWidth
+    carouselInner.style.transform = `translateX(${slideWidth}px)`;
+    carouselArrowRight.style.display = ''
+    if(slideWidth == 0) {
+      carouselArrowLeft.style.display = 'none'
+    } 
+  })
 }
